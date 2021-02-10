0889201ca012f676895fffc4e01a768b1dabedf0
Olivier <rodomond.o@gmail.com>
2021-02-10 21:01:42 +0700

    Use a wildcard on the /welcome route

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -2,7 +2,7 @@ const  { Welcome } = require('../services')
 
 module.exports = function(server, opts, next) {
   const { options } = opts
-  server.get('/', (request, reply) => {
+  server.get('*', (request, reply) => {
     reply
       .code(200)
       .send(Welcome.get())
